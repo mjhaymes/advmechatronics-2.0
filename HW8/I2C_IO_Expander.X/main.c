32476c519ed71cd58f903f26c96283e65575387b
Michael Haymes <MichaelHaymes2021@u.northwestern.edu>
2021-04-30 17:18:19 -0500

    upload of hw7 and hw8 materials

---
+++ HW8/I2C_IO_Expander.X/main.c
@@ -0,0 +1,84 @@
+#include "i2c_master_noint.h"
+
+// DEVCFG0
+#pragma config DEBUG = OFF // disable debugging
+#pragma config JTAGEN = OFF // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // disable flash write protect
+#pragma config BWP = OFF // disable boot write protect
+#pragma config CP = OFF // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL // use internal oscillator with pll
+#pragma config FSOSCEN = OFF // disable secondary oscillator
+#pragma config IESO = OFF // disable switching clocks
+#pragma config POSCMOD = OFF // high speed crystal mode (off))
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576 // use largest wdt
+#pragma config WINDIS = OFF // use non-window mode wdt
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+
+int main() {
+
+    __builtin_disable_interrupts(); // disable interrupts while initializing things
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0; // Configure A4 as the "heartbeat" output
+    LATAbits.LATA4 = 0; // A4 is initially off
+    TRISBbits.TRISB4 = 1; // B4 is an input
+    
+    i2c_master_setup(); // setup I2C stuff
+    
+    setRegister(IODIRA,0x00);       // make all A pins outputs
+    setRegister(IODIRB,0xFF);       // make all B pins inputs
+
+    __builtin_enable_interrupts();
+    
+    short int counter=0;
+    while (1) {
+        // checking if button is pushed or not; making LED turn on/off accordingly
+        switch (readRegister(GPIOB)&0b00000001) {
+            case 0: // if pin B0 is low (the button is pushed)
+                setRegister(OLATA,0b10000000);  // make pin GPA7 output high
+                break;
+            case 1: // if pin B0 is high (the button is not pushed)
+                setRegister(OLATA,0b00000000);  // make pin GPA7 output low
+                break;
+        }
+        
+        // LED heartbeat --> inverts pin A4 on the PIC every 1000 loops
+        counter++;
+        switch (counter) {
+            case 1750:
+                // don't wanna blink the heartbeat too fast so we'll wait like 1000
+                // loops through the infinite loop before inverting A4 on the PIC)
+                counter = 0;
+                LATAbits.LATA4 = !LATAbits.LATA4;
+        }
+    }
+}
